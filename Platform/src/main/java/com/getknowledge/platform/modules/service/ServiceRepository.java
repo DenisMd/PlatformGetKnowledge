1c7e3517bf3ffb244a21da9676a6f86f9eb6561e
DenisMD <markovdenis2012@gmail.com>
2015-10-31 17:56:30 +0500

    (change) service info

--- Platform/src/main/java/com/getknowledge/platform/modules/service/ServiceRepository.java
+++ Platform/src/main/java/com/getknowledge/platform/modules/service/ServiceRepository.java
@@ -4,10 +4,26 @@ import com.getknowledge.platform.base.repositories.TransientRepository;
 import com.getknowledge.platform.initializer.InitApplication;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
 
 @Repository("ServiceRepository")
 public class ServiceRepository extends TransientRepository<Service> {
 
+    @Autowired
+    InitApplication application;
+
+    @PostConstruct
+    public void ServiceRepository(){
+
+        for (String serviceName : application.getServices().keySet()) {
+            Service service = new Service();
+            service.setName(serviceName);
+            super.list.add(service);
+        }
+    }
 }
