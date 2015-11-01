a019a10cd61d7292e79f8ed3f9081c364909f854
DenisMD <markovdenis2012@gmail.com>
2015-11-01 12:44:20 +0500

    (incomplete) Task service получает задачи, по выборке в jpql

--- Platform/src/main/java/com/getknowledge/platform/modules/task/TaskService.java
+++ Platform/src/main/java/com/getknowledge/platform/modules/task/TaskService.java
@@ -1,19 +1,35 @@
 package com.getknowledge.platform.modules.task;
 
 import com.getknowledge.platform.base.services.AbstractService;
+import com.getknowledge.platform.modules.task.enumerations.TaskStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.TemporalType;
+import java.util.Calendar;
 
 @Service("TaskService")
 public class TaskService extends AbstractService {
 
+    @Autowired
+    TaskRepository taskRepository;
+
     public void startup() {
+        Task task = new Task();
+        task.setStartDate(Calendar.getInstance());
+        task.setServiceName("Wiii");
+        task.setTaskName("Defaukt");
+        task.setTaskStatus(TaskStatus.NotStarted);
+        taskRepository.create(task);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true) {
                     try {
                         Thread.sleep(10000);
-                        System.err.println("Start up");
+                        int size = entityManager.createQuery("select task from Task task where task.startDate < :today")
+                                .setParameter("today" , Calendar.getInstance(), TemporalType.TIMESTAMP).getResultList().size();
+                        System.err.println("Tasks : " + size);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
