9a1f6095b342f760e4512530daa6679daf396f1c
DenisMD <markovdenis2012@gmail.com>
2015-12-10 21:53:20 +0500

    (change) переводы и рефакторинг

--- Server/src/main/webapp/resources/application/module/section/bootstrap/bootstrap.js
+++ Server/src/main/webapp/resources/application/module/section/bootstrap/bootstrap.js
@@ -4,14 +4,14 @@ model.controller("bootstrapCtrl", function ($scope, $state,$http,applicationServ
         columns : ["id", "name", "bootstrapState", "order", "repeat"],
         content : [],
         tabs : [{
-            title : "Service",
+            title : "service",
             columns : [{name : "id" , "type" : "number", disabled : true} , {name : "name" , "type" : "string", disabled : true},{name : "bootstrapState" , "type" : "string", disabled : true},{name : "errorMessage" , "type" : "string" , disabled : true},{name : "stackTrace" , "type" : "textarea" , disabled : true}],
             readOnly : true
         } , {
             title : "doBootstrap",
             actionName : "do",
             className : className.bootstrap_services,
-            columns : [{name : "domain" , type : "text"} ,{name : "email" , type : "text"},{name : "password" , type : "text"},{name : "firstName" , type : "text"},{name : "lastName" , type : "text"} ],
+            columns : [{name : "domain" , type : "text"} ,{name : "email" , type : "text"},{name : "password" , type : "text"},{name : "firstName" , type : "text"},{name : "lastName" , type : "text"},{name : "initPassword" , type : "text"}],
             buttonText  : "doBootstrap"
 
         }]
