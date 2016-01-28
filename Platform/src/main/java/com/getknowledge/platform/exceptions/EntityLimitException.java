906f3be3a6b4967c5d81ad49e8226519a70ae7a1
dmarkov <markovdenis2012@gmail.com>
2016-01-28 11:56:10 +0500

    (change) Entity limit добавлен

---
+++ Platform/src/main/java/com/getknowledge/platform/exceptions/EntityLimitException.java
@@ -0,0 +1,19 @@
+package com.getknowledge.platform.exceptions;
+
+import com.getknowledge.platform.modules.trace.TraceService;
+import com.getknowledge.platform.modules.trace.trace.level.TraceLevel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE)
+public class EntityLimitException  extends PlatformException {
+    public EntityLimitException(String message) {
+        super(message);
+        super.errorResource.setStatus(HttpStatus.NOT_FOUND);
+    }
+
+    public EntityLimitException(String message, TraceService traceService, TraceLevel traceLevel) {
+        super(message, traceService, traceLevel);
+        super.errorResource.setStatus(HttpStatus.NOT_FOUND);
+    }
+}
