2fcdc27c49c0be5eeebde7b9667846c2bc96b625
dmarkov <markovdenis2012@gmail.com>
2016-02-20 14:08:29 +0500

    (incomplete) курсы

--- Server/src/main/webapp/resources/application/module/section/groupCourses/groupCourses.js
+++ Server/src/main/webapp/resources/application/module/section/groupCourses/groupCourses.js
@@ -1,6 +1,7 @@
 model.controller("groupCoursesCtrl", function ($scope,applicationService,className,pageService,$state) {
 
     var sectionName = pageService.getPathVariable("section",$state.params.path);
+    $scope.groupCoursesName = pageService.getPathVariable("groupCourses" , $state.params.path);
 
     $scope.folderData = {
         sectionName : sectionName,
