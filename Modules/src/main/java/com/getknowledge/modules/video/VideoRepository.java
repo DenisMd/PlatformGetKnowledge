b63b21e83097baad56e7a5cee31d693d3a5b79ad
dmarkov <markovdenis2012@gmail.com>
2016-03-24 17:59:55 +0500

    (refactor) video service

--- Modules/src/main/java/com/getknowledge/modules/video/VideoRepository.java
+++ Modules/src/main/java/com/getknowledge/modules/video/VideoRepository.java
@@ -1,5 +1,6 @@
 package com.getknowledge.modules.video;
 
+import com.getknowledge.modules.userInfo.UserInfo;
 import com.getknowledge.platform.base.repositories.BaseRepository;
 import com.getknowledge.platform.exceptions.DeleteException;
 import com.getknowledge.platform.exceptions.PlatformException;
@@ -8,8 +9,12 @@ import com.getknowledge.platform.modules.trace.trace.level.TraceLevel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Repository("VideoRepository")
 public class VideoRepository extends BaseRepository<Video> {
@@ -20,7 +25,7 @@ public class VideoRepository extends BaseRepository<Video> {
     }
 
     @Value("${video.path}")
-    private String pathToVideo;
+    public String pathToVideo;
 
     @Override
     public void remove(Long id) throws PlatformException {
@@ -42,5 +47,67 @@ public class VideoRepository extends BaseRepository<Video> {
         video.setVideoName(name);
         video.setAllowEveryOne(true);
         create(video);
+    }
+
+    //Загрузка видео в каталог
+    // {paramPath}/{userId}/{videoId}_{fileName}
+    public void uploadVideo(Video video,UserInfo userInfo,MultipartFile multipartFile) {
+
+        String separator = File.separator;
+
+        File file = new File(pathToVideo + separator + userInfo.getId());
+
+        if (!file.exists()) {
+            trace.log("Create dir for author videos : " + file.getAbsolutePath(), TraceLevel.Event);
+            file.mkdir();
+        }
+
+        //Если мы перезаливаем видео необходимо удалить старое
+        if (video.getLink() != null) {
+            File oldVideo = new File(pathToVideo + separator + video.getLink());
+            oldVideo.delete();
+        }
+
+        String link =  userInfo.getId() + separator +
+                       video.getId() + "_" +
+                       multipartFile.getOriginalFilename();
+
+        File videoFile = new File( pathToVideo + separator + link);
+
+        //Если такого файла нету созадем
+        if (!videoFile.exists()) {
+            try {
+                videoFile.createNewFile();
+            } catch (IOException e) {
+                trace.logException("Error upload video file" , e,TraceLevel.Error);
+            }
+        }
+
+        //Записываем данные из request
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(videoFile);
+            fos.write(multipartFile.getBytes());
+        } catch (IOException e) {
+            trace.logException("Error upload video file" , e,TraceLevel.Error);
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    trace.logException("Error upload video file" , e,TraceLevel.Error);
+                }
+            }
+        }
+
+        video.setLink(link);
+        merge(video);
+        trace.log("Video file successfully upload + " + videoFile.getAbsolutePath() , TraceLevel.Event);
+    }
+
+    public String getVideoPath(Long id){
+        Video video = read(id);
+        return video == null ? null : pathToVideo + File.separator + video.getLink();
     }
 }
--- Modules/src/main/java/com/getknowledge/modules/video/VideoService.java
+++ Modules/src/main/java/com/getknowledge/modules/video/VideoService.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -37,9 +38,6 @@ public class VideoService extends AbstractService implements BootstrapService , 
 
     @Autowired
     private UserInfoService userInfoService;
-
-    @Value("${video.path}")
-    private String pathToVideo;
 
     @Override
     public void bootstrap(HashMap<String, Object> map) throws Exception {
@@ -63,71 +61,26 @@ public class VideoService extends AbstractService implements BootstrapService , 
     }
 
     @ActionWithFile(name = "uploadVideo" , mandatoryFields = {"videoId"})
+    @Transactional
     public Result uploadVideo(HashMap<String,Object> data, List<MultipartFile> fileList) {
-        Long videoId = new Long((Integer)data.get("videoId"));
+        Long videoId = longFromField("videoId",data);
         Video video = videoRepository.read(videoId);
         if (video == null){
             return Result.Failed();
         }
         UserInfo userInfo = userInfoService.getAuthorizedUser(data);
-        if (userInfo == null || !video.getAuthorizationList().isAccessCreate(userInfo.getUser())) {
+        if (userInfo == null || !isAccessToEdit(data,video)) {
             return Result.AccessDenied();
         }
 
-        String separator = File.separator;
-
-        File file = new File(pathToVideo + separator + userInfo.getId());
-        if (!file.exists()) {
-            trace.log("Create dir for user : " + file.getAbsolutePath(), TraceLevel.Event);
-            file.mkdir();
-        }
-
-        MultipartFile multipartFile = fileList.get(0);
-
-        if (video.getLink() != null) {
-            File oldVideo = new File(pathToVideo + File.separator + video.getLink());
-            oldVideo.delete();
-        }
-
-        File videoFile = new File(pathToVideo + separator + userInfo.getId() + separator + videoId + "_" + multipartFile.getOriginalFilename());
-        if (!videoFile.exists()) {
-            try {
-                videoFile.createNewFile();
-            } catch (IOException e) {
-                trace.logException("Error upload video file" , e,TraceLevel.Error);
-            }
-        }
-
-        FileOutputStream fos = null;
-
-        try {
-            fos = new FileOutputStream(videoFile);
-            fos.write(multipartFile.getBytes());
-        } catch (FileNotFoundException e) {
-            trace.logException("Error upload video file" , e,TraceLevel.Error);
-        } catch (IOException e) {
-            trace.logException("Error upload video file" , e,TraceLevel.Error);
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    trace.logException("Error upload video file" , e,TraceLevel.Error);
-                }
-            }
-        }
-
-        video.setLink(userInfo.getId() + separator + videoId + "_" + multipartFile.getOriginalFilename());
-        videoRepository.merge(video);
-        trace.log("Video file successfully upload + " + videoFile.getAbsolutePath() , TraceLevel.Event);
+        videoRepository.uploadVideo(video,userInfo,fileList.get(0));
 
         return Result.Complete();
     }
 
     @Override
     public String getVideoLink(long id) {
-        Video video = videoRepository.read(id);
-        return video == null ? null : pathToVideo + File.separator + video.getLink();
+        return videoRepository.getVideoPath(id);
     }
 
     @Override
