03003db3313d272dbcf82c9568981831e62a0f60
dmarkov <markovdenis2012@gmail.com>
2016-03-28 10:28:26 +0500

    (refactor)

--- Modules/src/main/java/com/getknowledge/modules/books/BookRepository.java
+++ Modules/src/main/java/com/getknowledge/modules/books/BookRepository.java
@@ -1,13 +1,63 @@
 package com.getknowledge.modules.books;
 
+import com.getknowledge.modules.books.group.GroupBooks;
+import com.getknowledge.modules.books.tags.BooksTag;
+import com.getknowledge.modules.books.tags.BooksTagRepository;
+import com.getknowledge.modules.dictionaries.language.Language;
 import com.getknowledge.platform.base.repositories.ProtectedRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("BookRepository")
 public class BookRepository extends ProtectedRepository<Book> {
 
+    @Autowired
+    private BooksTagRepository booksTagRepository;
+
+
     @Override
     protected Class<Book> getClassEntity() {
         return Book.class;
+    }
+
+    private void addBookToTag(Book book) {
+        for (BooksTag booksTag : book.getTags()) {
+            booksTag.getBooks().add(book);
+            booksTagRepository.merge(booksTag);
+        }
+    }
+
+    public Book createBook(GroupBooks groupBooks,String name,String description,Language language,List<String> links,List<String> tags) {
+        Book book = new Book();
+        book.setGroupBooks(groupBooks);
+        book.setName(name);
+        book.setDescription(description);
+        book.setLanguage(language);
+        if (links != null)
+            book.setLinks(links);
+        if (tags != null)
+            booksTagRepository.createTags(tags,book);
+
+        create(book);
+        addBookToTag(book);
+        return book;
+    }
+
+    public Book updateBook(Book book,String name,String description,List<String> links,List<String> tags) {
+        book.setName(name);
+        book.setDescription(description);
+        if (links != null)
+            book.setLinks(links);
+        if (tags != null) {
+            booksTagRepository.removeTagsFromEntity(book);
+            booksTagRepository.createTags(tags, book);
+        }
+
+        merge(book);
+        addBookToTag(book);
+        booksTagRepository.removeUnusedTags();
+        return book;
     }
 }
