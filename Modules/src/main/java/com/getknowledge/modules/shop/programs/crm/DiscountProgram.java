cbe5f7c72c1c798f71cab0bf0a4da66e9e952d45
dmarkov <markovdenis2012@gmail.com>
2016-04-01 13:41:12 +0500

    (incomplete) prices

--- Modules/src/main/java/com/getknowledge/modules/shop/programs/crm/DiscountProgram.java
+++ Modules/src/main/java/com/getknowledge/modules/shop/programs/crm/DiscountProgram.java
@@ -1,20 +1,23 @@
 package com.getknowledge.modules.shop.programs.crm;
 
+import com.getknowledge.modules.shop.item.Item;
 import com.getknowledge.platform.base.entities.AbstractEntity;
 import com.getknowledge.platform.base.entities.AuthorizationList;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table(name = "crm_discounts_program")
+@Table(name = "crm_discount_programs")
 public class DiscountProgram extends AbstractEntity {
 
     private String name;
 
     @Column(length = 1000)
     private String note;
+
+    @ManyToMany
+    private List<Item> items;
 
     @Override
     public AuthorizationList getAuthorizationList() {
