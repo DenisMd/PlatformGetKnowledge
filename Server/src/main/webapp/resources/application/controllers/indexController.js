4d9614c57d8293aa70547221d40c962e05f7bebc
DenisMD <markovdenis2012@gmail.com>
2016-04-06 20:33:29 +0500

    (refactor) slider

--- Server/src/main/webapp/resources/application/controllers/indexController.js
+++ Server/src/main/webapp/resources/application/controllers/indexController.js
@@ -1,2 +1,28 @@
 model.controller("indexController",function($scope){
+    //--------------------------------------------- опции слайдера
+    $scope.carouselData = {
+        interval : 5000,
+        slides : [
+            {
+                section: "Programming",
+                image: "/resources/image/index/slider/programming.jpg",
+                text: "carousel_programming"
+            },
+            {
+                section: "Math",
+                image: "/resources/image/index/slider/math.jpg",
+                text: "carousel_math"
+            },
+            {
+                section: "Physic",
+                image: "/resources/image/index/slider/physic.jpg",
+                text: "carousel_physic"
+            },
+            {
+                section: "Design",
+                image: '/resources/image/index/slider/design.jpg',
+                text: "carousel_design"
+            }
+        ]
+    };
 });
--- Server/src/main/webapp/resources/application/getKnowledge.js
+++ Server/src/main/webapp/resources/application/getKnowledge.js
@@ -230,34 +230,6 @@ model.controller("mainController", function ($scope,$rootScope, $http, $state, a
         }
         return result;
     };
-
-
-    //--------------------------------------------- опции слайдера
-    $scope.carouselData = {
-        interval : 5000,
-        slides : [
-            {
-                section: "Programming",
-                image: "/resources/image/index/slider/programming.jpg",
-                text: "carousel_programming"
-            },
-            {
-                section: "Math",
-                image: "/resources/image/index/slider/math.jpg",
-                text: "carousel_math"
-            },
-            {
-                section: "Physic",
-                image: "/resources/image/index/slider/physic.jpg",
-                text: "carousel_physic"
-            },
-            {
-                section: "Design",
-                image: '/resources/image/index/slider/design.jpg',
-                text: "carousel_design"
-            }
-        ]
-    };
 });
 
 model.controller("treeListCtrl" , function ($scope) {
