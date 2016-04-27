9fe974aad9826776fecdd1672db698bd6d14944a
dmarkov <markovdenis2012@gmail.com>
2016-04-27 13:58:37 +0500

    (refactor)  custom filter

--- Server/src/main/webapp/resources/application/controllers/dialogs/customFilterController.js
+++ Server/src/main/webapp/resources/application/controllers/dialogs/customFilterController.js
@@ -1,3 +1,10 @@
 model.controller("customFilterController",function($scope,customFilterService){
 
+    $scope.saveFilter = function(){
+
+    };
+
+    $scope.closeModal = function(){
+        customFilterService.closeDialog();
+    };
 });
--- Server/src/main/webapp/resources/application/services/customFilterService.js
+++ Server/src/main/webapp/resources/application/services/customFilterService.js
@@ -3,6 +3,12 @@ model.factory('customFilterService', function() {
     return {
         openDialog : function(){
             $(modalId).modal('show');
+        },
+        closeDialog : function(filter) {
+            //if (element){
+            //    callbackSave(element);
+            //}
+            $(modalId).modal("hide");
         }
     }
 });
