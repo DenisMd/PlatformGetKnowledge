e005f118bc8f3d1372276ab583e69241a1fe050f
alenaaa <v_al_d@mail.ru>
2016-05-18 22:31:47 +0500

    (minor changes) анимация

--- Server/src/main/webapp/resources/application/controllers/selectors/serverSelectorController.js
+++ Server/src/main/webapp/resources/application/controllers/selectors/serverSelectorController.js
@@ -167,44 +167,62 @@ model.controller("serverSelectorController" , function ($scope , customFilterSer
     }
 });
 
-model.animation('.trSlide', [function () {
-    var item = null;
-    var speedUp = 3;
-    var speedDown = 3;
+model.animation('.trSlide', [function() {
+    var shrinkyItems=[];
+    var speedUp=3;
+    var speedDown=3;
 
-    function initShrinky(element, direction, doneFn) {
-        var child = element.getElementsByTagName('div')[0];
-        var targetHeight = direction == "up" ? 0 : child.offsetHeight;
-        var currentHeight = direction == "up" ? child.offsetHeight : 0;
-        item = {
-            element: element,
-            direction: direction,
+    function clear(){
+        var needClear = true;
+        shrinkyItems.forEach(function(item, index, object) {
+            if (item !== null) {
+                needClear = false;
+            }
+        });
+        if (needClear){
+            shrinkyItems = [];
+        }
+    }
+
+    function initShrinky(element, direction, doneFn){
+        clear();
+        var child=element.getElementsByTagName('div')[0];
+        var targetHeight=direction=="up"?0 : child.offsetHeight;
+        var currentHeight = direction=="up"?child.offsetHeight:0;
+        console.log(direction + " "+ targetHeight + currentHeight);
+
+        var id=shrinkyItems.push({
+            element:element,
+            direction:direction,
             currentHeight: currentHeight,
             targetHeight: targetHeight,
-            doneFn: doneFn
-        };
-        if (direction == "down")
-            element.style.height = (currentHeight) + "px";
+            doneFn:doneFn
+        });
+        console.log(id);
+        if(direction=="down")
+            element.style.height=(currentHeight)+"px";
 
-        setTimeout(function () {
-            shrinky();
+        setTimeout(function(){
+            shrinky(id-1);
         });
     }
 
-    function shrinky() {
-        var heightChange = item.currentHeight < item.targetHeight ? speedDown : -speedUp;
-        item.currentHeight += heightChange;
-        if (item.direction == "down" && item.currentHeight > item.targetHeight)
-            item.currentHeight = item.targetHeight;
-        if (item.direction == "up" && item.currentHeight < item.targetHeight)
-            item.currentHeight = item.targetHeight;
-        item.element.style.height = (item.currentHeight) + "px";
-        if (item.currentHeight == item.targetHeight) {
+    function shrinky(id){
+        var item=shrinkyItems[id];
+        var heightChange=item.currentHeight<item.targetHeight?speedDown:-speedUp;
+        item.currentHeight+=heightChange;
+        if(item.direction=="down" && item.currentHeight>item.targetHeight)
+            item.currentHeight=item.targetHeight;
+        if(item.direction=="up" && item.currentHeight<item.targetHeight)
+            item.currentHeight=item.targetHeight;
+        item.element.style.height=(item.currentHeight)+"px";
+        if (item.currentHeight==item.targetHeight){
             item.doneFn();
-            item = null;
-        } else {
-            setTimeout(function () {
-                shrinky();
+            shrinkyItems[id]=null;
+        }
+        else{
+            setTimeout(function(){
+                shrinky(id);
             });
         }
     }
@@ -213,14 +231,15 @@ model.animation('.trSlide', [function () {
     return {
         // make note that other events (like addClass/removeClass)
         // have different function input parameters
-        enter: function (element, doneFn) {
-            var div = element[0].getElementsByClassName("item-panel")[0];
-            initShrinky(div, "down", doneFn);
+        enter: function(element, doneFn) {
+            var div=element[0].getElementsByClassName("item-panel")[0];
+            initShrinky(div,"down", doneFn);
         },
 
-        leave: function (element, doneFn) {
-            var div = element[0].getElementsByClassName("item-panel")[0];
-            initShrinky(div, "up", doneFn);
+
+        leave: function(element, doneFn) {
+           var div=element[0].getElementsByClassName("item-panel")[0];
+            initShrinky(div,"up", doneFn);
         }
     }
 }]);
