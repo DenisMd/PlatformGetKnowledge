9f425b981246af27a8e4df8d7f83e576b7834756
DenisMD <markovdenis2012@gmail.com>
2016-05-28 18:44:12 +0500

    (change) верстка input для folders

--- Server/src/main/webapp/resources/application/module/section/groupCourses/groupCourses.js
+++ Server/src/main/webapp/resources/application/module/section/groupCourses/groupCourses.js
@@ -6,7 +6,8 @@ model.controller("groupCoursesCtrl", function ($scope,applicationService,classNa
     if (!$scope.groupCourses) {
         $scope.folderData = {
             sectionName: sectionName,
-            className: className.groupCourses
+            className: className.groupCourses,
+            title: $scope.translate("courses_title")
         };
     } else {
         $scope.coursesData = {
