3e3ce3a139b2995dfd0990da905c1a220e6331ed
dmarkov <markovdenis2012@gmail.com>
2016-06-01 13:49:50 +0500

    (mafjor improvment) первая версия папок

--- Server/src/main/webapp/resources/application/controllers/cards/foldersController.js
+++ Server/src/main/webapp/resources/application/controllers/cards/foldersController.js
@@ -1,16 +1,51 @@
 model.controller("foldersController" , function ($scope,applicationService) {
 
     var numberFoldersInRow = 4;
+    var tempArr = [];
+
+    $scope.currentFilterByDate = true;
 
     $scope.filter = applicationService.createFilter($scope.getData().className,0,10);
     $scope.filter.createFiltersInfo();
     $scope.filter.equals("section.name","text",$scope.getData().sectionName);
 
-    //filter.addCustomFilter("orderByCount" , {});
+    $scope.by_date = function() {
+        $scope.currentFilterByDate = true;
+        $scope.filter.clearOrder();
+        $scope.filter.clearCustomFilters();
+        $scope.filter.setOrder("createDate" , true);
 
-    var tempArr = [];
-    $scope.foldersGroup = [tempArr];
+        tempArr = [];
+        $scope.foldersGroup = [tempArr];
 
+        doAction();
+    };
+
+    $scope.by_count = function() {
+        $scope.currentFilterByDate = false;
+        $scope.filter.clearOrder();
+        $scope.filter.clearCustomFilters();
+        $scope.filter.addCustomFilter("orderByCount" , {});
+
+        tempArr = [];
+        $scope.foldersGroup = [tempArr];
+
+        doAction();
+    };
+    var likeIndex;
+    $scope.searchFilter = function(text) {
+        if (likeIndex != undefined) {
+            $scope.filter.result.filtersInfo.filters.splice(likeIndex,1);
+        }
+        if (text) {
+            likeIndex = $scope.filter.like("title", "text", "%" + text + "%");
+        }
+
+        tempArr = [];
+        $scope.foldersGroup = [tempArr];
+
+        doAction();
+    };
 
     var addLog = function(folder){
         folder.imgSrc = applicationService.imageHref($scope.getData().className,folder.id);
@@ -26,7 +61,7 @@ model.controller("foldersController" , function ($scope,applicationService) {
         applicationService.filterRequest($scope,"foldersInfo",$scope.filter,addLog);
     };
 
-    doAction();
+    $scope.by_date();
 
     $scope.loadMore = function () {
         $scope.filter.increase(10);
